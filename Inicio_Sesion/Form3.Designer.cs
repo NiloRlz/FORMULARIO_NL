3fe121c8d91ac554870778ae59f481bd7c0e4138
NiloRlz <1459566@senati.pe>
2023-09-08 18:49:56 -0500

    ActDiseño 3.2

--- Inicio_Sesion/Form3.Designer.cs
+++ Inicio_Sesion/Form3.Designer.cs
@@ -32,7 +32,7 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnURL = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -71,18 +71,19 @@
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
-            // button1
+            // btnURL
             // 
-            this.button1.BackColor = System.Drawing.Color.DodgerBlue;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(520, 389);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(126, 49);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "MATRICULAR";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btnURL.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnURL.FlatAppearance.BorderSize = 0;
+            this.btnURL.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnURL.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnURL.Location = new System.Drawing.Point(520, 389);
+            this.btnURL.Name = "btnURL";
+            this.btnURL.Size = new System.Drawing.Size(126, 49);
+            this.btnURL.TabIndex = 4;
+            this.btnURL.Text = "MATRICULAR";
+            this.btnURL.UseVisualStyleBackColor = false;
+            this.btnURL.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // textBox1
             // 
@@ -102,7 +103,7 @@
             this.BackColor = System.Drawing.Color.DarkGray;
             this.ClientSize = new System.Drawing.Size(658, 450);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnURL);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -122,7 +123,7 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnURL;
         private System.Windows.Forms.TextBox textBox1;
     }
 }
--- Inicio_Sesion/Form3.cs
+++ Inicio_Sesion/Form3.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -18,5 +19,11 @@ namespace inicio_de_sesion_2
         }
         //holitas
         private void button1_Click(object sender, EventArgs e) => this.Close();
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            ProcessStartInfo sInfo = new ProcessStartInfo("https://admisionsenati.com/?gad=1&gclid=EAIaIQobChMIws6iypmcgQMVC-mRCh1M9ws5EAAYASAAEgIRs_D_BwE");
+            Process.Start(sInfo);
+        }
     }
 }
